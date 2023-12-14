2a6572e02ddbd348977226b45e0408cc72b07656
Roland Kofler <github@gegenwart.today>
2023-12-14 14:51:17 +0100

    (feat) Paper trading with real ticker (feat) logging

---
+++ laxmi/PaperExchange.py
@@ -0,0 +1,68 @@
+import logging
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+class PaperExchange:
+    def __init__(self, exchange_class):
+        self.balances = {'BTC': 0.01, 'ETH': 0.10, 'EUR': 100}  # Instance-level balance
+        self.exchange = exchange_class()  # Initialize real exchange class for data
+        self.order_book = []
+
+    def fetch_ticker(self, symbol):
+        try:
+            # Fetch real ticker data from the exchange
+            ticker = self.exchange.fetch_ticker(symbol)
+            return ticker
+        except Exception as e:
+            logging.error(f"Error fetching ticker: {e}")
+            return None
+
+    def create_market_buy_order(self, symbol, amount):
+        # Simulate market buy order
+        ticker = self.fetch_ticker(symbol)
+        if ticker is None:
+            return {'info': 'Failed to fetch market data for order simulation'}
+
+        simulated_price = ticker['last']
+        cost = simulated_price * amount
+        base_currency, quote_currency = symbol.split('/')
+
+        # Check if enough balance is available
+        if self.balances.get(quote_currency, 0) < cost:
+            return {'info': 'Insufficient balance for market buy order'}
+
+        self.balances[quote_currency] -= cost
+        self.balances[base_currency] += amount
+        self.order_book.append({
+            'symbol': symbol,
+            'type': 'market',
+            'side': 'buy',
+            'amount': amount,
+            'price': simulated_price
+        })
+        return {'info': 'Market buy order simulated', 'filled': amount, 'cost': cost}
+
+    def create_market_sell_order(self, symbol, amount):
+        # Simulate market sell order
+        ticker = self.fetch_ticker(symbol)
+        if ticker is None:
+            return {'info': 'Failed to fetch market data for order simulation'}
+
+        simulated_price = ticker['last']
+        revenue = simulated_price * amount
+        base_currency, quote_currency = symbol.split('/')
+
+        # Check if enough of the base currency is available to sell
+        if self.balances.get(base_currency, 0) < amount:
+            return {'info': 'Insufficient amount of base currency for market sell order'}
+
+        self.balances[base_currency] -= amount  # Reduce the base currency
+        self.balances[quote_currency] += revenue  # Increase the quote currency
+        self.order_book.append({
+            'symbol': symbol,
+            'type': 'market',
+            'side': 'sell',
+            'amount': amount,
+            'price': simulated_price
+        })
+        return {'info': 'Market sell order simulated', 'filled': amount, 'revenue': revenue}
--- laxmi/start.py
+++ laxmi/start.py
@@ -1,16 +1,28 @@
-import ccxt
+import logging
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 import json
-import time
+import ccxt
+
+from laxmi.PaperExchange import PaperExchange
+
 
 class MultiExchangeCryptoBot:
-    def __init__(self, exchange_credentials):
+    def __init__(self, exchange_credentials, paper_trading_config):
         self.exchanges = {}
         for exchange_id, credentials in exchange_credentials.items():
             exchange_class = getattr(ccxt, exchange_id)
-            self.exchanges[exchange_id] = exchange_class({
-                'apiKey': credentials['api_key'],
-                'secret': credentials['api_secret']
-            })
+            if paper_trading_config.get(exchange_id, False):
+                # Initialize a paper trading exchange
+                self.exchanges[exchange_id] = PaperExchange(exchange_class=exchange_class)
+            else:
+                # Initialize a real exchange
+
+                self.exchanges[exchange_id] = exchange_class({
+                    'apiKey': credentials['api_key'],
+                    'secret': credentials['api_secret']
+                })
 
     def fetch_price(self, symbol):
         prices = {}
@@ -19,7 +31,7 @@ class MultiExchangeCryptoBot:
                 ticker = exchange.fetch_ticker(symbol)
                 prices[exchange_id] = ticker['last']
             except Exception as e:
-                print(f"Error fetching price from {exchange_id}: {e}")
+                logging.error(f"Error fetching price from {exchange_id}: {e}")
         return prices
 
     def place_market_order(self, symbol, order_type, amount):
@@ -34,21 +46,28 @@ class MultiExchangeCryptoBot:
                     raise ValueError('Order type must be "buy" or "sell"')
                 responses[exchange_id] = response
             except Exception as e:
-                print(f"Error placing order on {exchange_id}: {e}")
+                logging.error(f"Error placing order on {exchange_id}: {e}")
         return responses
 
     def run(self, symbol, order_type, amount):
         prices = self.fetch_price(symbol)
-        print("Current prices:", prices)
+        logging.info("Current prices: %s", json.dumps(prices, indent=4, sort_keys=True))
 
         orders = self.place_market_order(symbol, order_type, amount)
         # pretty print the response
-        print("Orders:", json.dumps(orders, indent=4, sort_keys=True))
+        logging.info("Orders: %s", json.dumps(orders, indent=4, sort_keys=True))
 
 
 # load Exchange credentials from a module
-from keys.do_not_share_credentials import dont_share_credentials
+from keys.do_not_share_credentials import dont_share_credentials as exchange_credentials
 
-# Instantiate and run the bot
-bot = MultiExchangeCryptoBot(dont_share_credentials())
-bot.run('BTC/EUR', 'buy', 0.0003)
+paper_trading_config = {
+    # Configuration to indicate which exchanges are for paper trading
+    'kraken': True,
+    'bitstamp': True,
+    'cryptocom': True
+}
+
+bot = MultiExchangeCryptoBot(exchange_credentials(), paper_trading_config)
+
+bot.run('BTC/EUR', 'sell', 0.0003)
